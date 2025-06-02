7607af3c7ff7703d9b868634ddeb8ad22b93e8c6
Saloni Kahar <salonikahar20@gmail.com>
2025-06-03 02:11:43 +0530

    route

--- app.js
+++ app.js
@@ -3,55 +3,67 @@ const express = require('express')
 const app = express();
 const port = 8001;
 const path = require('path')
+const fs = require('fs')
 const db = require('./config/db')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
 app.use(express.urlencoded())
 
-let data = require('./models/empModel');
+// let data = require('./models/empModel');
 const dataEmp = require('./models/empModel');
 
 app.get('/', async function (req, res) {
 
     let empData = await dataEmp.find();
-    
+
     return res.render('index',
         { "record": empData }
     );
 })
 
-app.post('/add',async function (req, res) {
+app.post('/add', dataEmp.fileUploads, async function (req, res) {
     // console.log(req.body)
     // data.push(req.body)
-    await data.create(req.body)
-
+    // console.log(req.body);
+    // console.log(req.file);
+    if (req.file) {
+        req.body.image = dataEmp.imagepath + '/' + req.file.filename;
+    }
+    await dataEmp.create(req.body);
     return res.redirect('/');
 })
 
-app.get('/deleteData/:pid', async(req, res) => {
+app.get('/deleteData/:pid', async (req, res) => {
 
     let empId = req.params.pid;
-
+    let singleData = await dataEmp.findById(empId)
+    if (singleData.image){
+        let imgPath = path.join(__dirname , singleData.image)
+        // console.log(imgPath)
+        await fs.unlinkSync(imgPath)
+    }
     // data.splice(req.params.pid, 1)
-    await data.findByIdAndDelete(empId)
+    await dataEmp.findByIdAndDelete(empId)
     return res.redirect('/');
 })
 
-app.get('/updateData',async (req, res) => {
+app.get('/updateData', async (req, res) => {
     // console.log(req.query.position);
     let index = req.query.position;
-    let singleData = await data.findById(index);
+    let singleData = await dataEmp.findById(index);
 
     return res.render('editform', {
-        singleData ,
+        singleData,
         index
     })
 })
 
 app.post('/editForm/:id', async (req, res) => {
     // data[req.body.index] = req.body;
-    await data.findByIdAndUpdate(req.params.id , req.body);
+    await dataEmp.findByIdAndUpdate(req.params.id, req.body);
     return res.redirect('/');
 })
 
--- models/empModel.js
+++ models/empModel.js
@@ -1,32 +1,53 @@
 let mongoose = require('mongoose');
+let path = require('path')
+let multer = require('multer');
+const { type } = require('os');
+let empImage = '/uploads';
+
 
 const empModel = mongoose.Schema({
     name: {
         type: String,
         require: true
-    } ,
+    },
     age: {
         type: Number,
         require: true
-    } ,
+    },
     gender: {
         type: String,
         require: true
-    } , 
-    hobby : {
-        type : Array , 
-        require : true
-    } , 
+    },
+    hobby: {
+        type: Array,
+        require: true
+    },
     city: {
         type: String,
         require: true
-    } ,
-    address : {
-        type : String,
-        require : true
-    } 
+    },
+    address: {
+        type: String,
+        require: true
+    },
+    image: {
+    type: String,
+    require: true
+    }
 })
 
-const dataEmp = mongoose.model('emp' , empModel)
+const empstorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '..', empImage))
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '_' + Date.now())
+    }
+})
+
+empModel.statics.fileUploads = multer({ storage: empstorage }).single('image')
+empModel.statics.imagepath = empImage ;
+
+const dataEmp = mongoose.model('emp', empModel)
 
 module.exports = dataEmp;
